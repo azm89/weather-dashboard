7a5c3057f4ed92cd74434268edbc25f3b5b62bd4
azm89 <ashtonmoore89@gmail.com>
2022-07-15 11:45:29 -0700

    started script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,31 @@
-var apiKey = "27755f1624a6ac8589dcc66052bdaf2e";
-cityName = "San Francisco";
+function initPage() {
+    //Grabbing page elements
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const history = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-// submit the form to fetch weather information
+    const APIKey = "27755f1624a6ac8589dcc66052bdaf2e":
 
-    // fetch the city name from the text <input>
+    function getWeather(cityName) {
+        let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}`
+    }
 
-    // call the `fetchGeolocation` and pass the city name
 
-// handle button clicks to fetch weather information
 
-    //get the city name from the clicked button's (event.target) data- attribute
 
-    // call the `fetchGeolocation` and pass the city name
 
-//fetch geolocation data (Geocoding API)
-function fetchGeolocation(cityName) { 
-    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=27755f1624a6ac8589dcc66052bdaf2e&limit=5`;
 
-    fetchGeolocation(request)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
 
-        //access lat and lon from data
 
-        //call fetchOneCallWeather and pass through the lat and lon
-    })
+
 }
-
-// fetchGeolocation("Seattle");
-//fetch weather data (Onecall)
-
-function fetchOneCallWeather(lat, lon) {
-    var lat = $("#lat-field").val();
-    var request = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=27755f1624a6ac8589dcc66052bdaf2e&units=imperial&exclude=hourly,minutely`
-
-        fetchGeolocation(request)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        //render / display the weather data
-    })
-    
-};
-console.log(request);
-
-// function myFunctA(){
-//     var number = 1;
-
-//     myFunctB(number);
-// }
-
-// function myFunctB(number) { 
-    
-// }
